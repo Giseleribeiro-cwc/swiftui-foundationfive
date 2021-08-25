fb9209f8e42386eda8d26ccfd4457e1a7a350952
Giseleribeiro-cwc <gisele_ribeiro@outlook.com.br>
2021-08-25 11:33:34 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Gisele Ribeiro on 8/25/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
